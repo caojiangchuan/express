ec8394a3ce34540606fc0c13aa33c50566ae3f72
曹江川 <v_caojiangchuan@duxiaoman.com>
2020-03-24 16:32:37 +0800

    模块化分割

---
+++ cjcproject/controller/nodemysql.js
@@ -0,0 +1,108 @@
+const connection = require('../bin/mysql');
+var sql = 'SELECT * FROM posts';
+
+connection.connect();
+
+// 查询语句
+const find = (req, res) => {
+    connection.query(sql, function (err, result) {
+        if (err) {
+            console.log('[SELECT ERROR] - ', err.message);
+            return;
+        }
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        res.header('Access-Control-Allow-Methods', '*');
+        res.header('Content-Type', 'application/json;charset=utf-8');
+        res.send({
+            data: result,
+            code: '0000',
+            status: 'success'
+        });
+
+    });
+};
+// 新增信息
+const add = (req, res) => {
+    // connection.connect();
+    let id = req.body.id;
+    let title = req.body.title;
+    var addSql = 'INSERT INTO posts(id,title) VALUES(?,?)';
+    var addSqlParams = [id, title];
+    connection.query(addSql, addSqlParams, function (err, result) {
+        for (let i = 0; i < addSqlParams.length; i++) {
+            if (addSqlParams[i] === '') {
+                res.sendStatus(400);
+                return;
+            }
+
+        }
+        if (err) {
+            console.log('[INSERT ERROR] - ', err.message);
+            return;
+        }
+
+        res.send({
+            data: '添加成功'
+        });
+        console.log('INSERT ID:', result);
+    });
+};
+// 更改数据
+const change = (req, res) => {
+    let id = req.body.id;
+    let title = req.body.title;
+    var modSql = 'UPDATE posts SET title = ? WHERE id = ?';
+    var modSqlParams = [title, id];
+    connection.query(modSql, modSqlParams, function (err, result) {
+        if (err) {
+            console.log('[UPDATE ERROR] - ', err.message);
+            return;
+        }
+
+        console.log('UPDATE affectedRows', result.affectedRows);
+        res.send(result);
+    });
+};
+// 删除数据
+const del = (req, res) => {
+    let id = req.query.id;
+    var delSql = `DELETE FROM posts where id=${id}`;
+    connection.query(delSql, function (err, result) {
+        if (err) {
+            console.log('[DELETE ERROR] - ', err.message);
+            return;
+        }
+
+        console.log('DELETE affectedRows', result.affectedRows);
+        res.send(result);
+    });
+};
+// 按需查找
+const choose = (req, res) => {
+    let id = req.query.id;
+    let title = req.query.title;
+    if (title) {
+        var delSql = `SELECT * FROM posts WHERE title LIKE '%${title}%'`; // 模糊搜索
+    }
+    else {
+        var delSql = 'SELECT * FROM posts';
+    }
+    connection.query(delSql, function (err, result) {
+        if (err) {
+            console.log('[DELETE ERROR] - ', err.message);
+            return;
+        }
+
+        console.log('DELETE affectedRows', result.affectedRows);
+        res.send(result);
+    });
+};
+module.exports = {
+    find,
+    add,
+    change,
+    del,
+    choose
+};
--- cjcproject/routes/index.js
+++ cjcproject/routes/index.js
@@ -1,148 +1,31 @@
-const connection = require('../bin/mysql');
 const express = require('express');
 var bodyParser = require('body-parser');
-// const app = express();
+const nodemysql = require('../controller/nodemysql');
 var router = express.Router();
-
-// console.log(con);
 
 /* post请求插件 */
 var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({
     extended: false
 });
-
-console.log(connection);
-
-var sql = 'SELECT * FROM posts';
 // 查
-connection.connect();
-
 router.get('/', (req, res) => {
-
     res.render('index', {title: '标题', message: '信息'});
 });
 
 /* 查数据 */
-router.get('/find', (req, res) => {
-
-    connection.query(sql, function (err, result) {
-        if (err) {
-            console.log('[SELECT ERROR] - ', err.message);
-            return;
-        }
-
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
-        res.header('Access-Control-Allow-Methods', '*');
-        res.header('Content-Type', 'application/json;charset=utf-8');
-        res.send({
-            data: result,
-            code: '0000',
-            status: 'success'
-        });
-
-    });
-});
+router.get('/find', nodemysql.find);
 
 /* 新增数据 */
-router.post('/add', urlencodedParser, (req, res) => {
-    // connection.connect();
-    let id = req.body.id;
-    let title = req.body.title;
-    var addSql = 'INSERT INTO posts(id,title) VALUES(?,?)';
-    var addSqlParams = [id, title];
-    connection.query(addSql, addSqlParams, function (err, result) {
-        for (let i = 0; i < addSqlParams.length; i++) {
-            if (addSqlParams[i] === '') {
-                res.sendStatus(400);
-                return;
-            }
-
-        }
-
-        if (err) {
-            console.log('[INSERT ERROR] - ', err.message);
-            return;
-        }
-
-        res.send({
-            data: '添加成功'
-        });
-
-        // console.log('INSERT ID:',result.insertId);        
-        console.log('INSERT ID:', result);
-    });
-});
+router.post('/add', urlencodedParser, nodemysql.add);
 
 /* 更改数据 */
-router.post('/change', urlencodedParser, (req, res) => {
-    // connection.connect();
-    let id = req.body.id;
-    let title = req.body.title;
-
-    var modSql = 'UPDATE posts SET title = ? WHERE id = ?';
-    var modSqlParams = [title, id];
-    // 改
-    connection.query(modSql, modSqlParams, function (err, result) {
-        if (err) {
-            console.log('[UPDATE ERROR] - ', err.message);
-            return;
-        }
-
-        console.log('UPDATE affectedRows', result.affectedRows);
-        res.send(result);
-
-    });
-});
+router.post('/change', urlencodedParser, nodemysql.change);
 
 /* 删除 */
-
-router.get('/del', (req, res) => {
-    // connection.connect();
-    let id = req.query.id;
-
-    var delSql = `DELETE FROM posts where id=${id}`;
-    // 删
-    connection.query(delSql, function (err, result) {
-        if (err) {
-            console.log('[DELETE ERROR] - ', err.message);
-            return;
-        }
-
-        console.log('DELETE affectedRows', result.affectedRows);
-        res.send(result);
-
-    });
-});
+router.get('/del', nodemysql.del);
 
 /* 按需查找 */
-
-router.get('/choose', (req, res) => {
-    // connection.connect();
-    let id = req.query.id;
-    let title = req.query.title;
-
-    if (title) {
-        // var delSql = `SELECT * FROM posts where id=${id}`;
-        var delSql = `SELECT * FROM posts WHERE title LIKE '%${title}%'`; // 模糊搜索
-    }
-    else {
-        var delSql = 'SELECT * FROM posts';
-    }
-
-    connection.query(delSql, function (err, result) {
-        if (err) {
-            console.log('[DELETE ERROR] - ', err.message);
-            return;
-        }
-
-        console.log('DELETE affectedRows', result.affectedRows);
-        res.send(result);
-
-    });
-});
-
+router.get('/choose', nodemysql.choose);
 // connection.end();
-
 module.exports = router;
